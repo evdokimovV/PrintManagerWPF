486b7c69fc638eb203414a9443bb4d214b3af601
evdokimovV <89418401+evdokimovV@users.noreply.github.com>
2021-11-17 09:58:25 +0500

    Add files via upload

---
+++ PrintManager/PrintManagerCommands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintManager
+{
+    public class PrintManagerCommands
+    {
+        static PrintManagerCommands()
+        {
+            CreateDoc = new System.Windows.Input.RoutedCommand("CreateDoc", typeof(MainWindow));
+        }
+        public static System.Windows.Input.RoutedCommand CreateDoc { get; set; }
+    }
+}
