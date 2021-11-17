2989fbce029a3e235b98f245b18e3f7a2b6dab27
evdokimovV <89418401+evdokimovV@users.noreply.github.com>
2021-11-17 09:54:25 +0500

    Add files via upload

---
+++ PrintManager/AddDocumentForm.xaml.cs
@@ -0,0 +1,51 @@
+﻿using PrintManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrintManager
+{
+    /// <summary>
+    /// Interaction logic for AddDocumentForm.xaml
+    /// </summary>
+    public partial class AddDocumentForm : Window
+    {
+        public DocGridItem NewDoc;
+        public AddDocumentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateDoc_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !string.IsNullOrEmpty(printTime?.Text) && !string.IsNullOrEmpty(docName?.Text);
+        }
+
+        private void CreateDoc_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            NewDoc = new DocGridItem(docName.Text, Convert.ToInt32(printTime.Text));
+            DialogResult = true;
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void PrintTime_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, e.Text.Length - 1))
+                e.Handled = true;
+        }
+    }
+}
---
+++ PrintManager/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using PrintManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrintManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public PrintManagerViewModel PrintManagerModel { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrintManagerModel = new PrintManagerViewModel();
+            //PrintManagerModel.AddDoc(new DocGridItem("qwe1", 1) { Id = 1 });
+            //PrintManagerModel.AddDoc(new DocGridItem("qwe2", 2) { Id = 2 });
+            //PrintManagerModel.AddDoc(new DocGridItem("qwe3", 3) { Id = 3 });
+            docList.ItemsSource = PrintManagerModel.Docs;
+        }
+
+        private async void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = false;
+            btnStop.IsEnabled = true;
+            await PrintManagerModel.StartPrinting();
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            PrintManagerModel.StopPrinting();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            PrintManagerModel.AddNewDoc();
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            PrintManagerModel.CancelDocPrint((DocGridItem)docList.SelectedItem);
+        }
+
+        private void BtnAvgPrintTime_Click(object sender, RoutedEventArgs e)
+        {
+            var printedDocs = PrintManagerModel.Docs.Where(s => s.Printed == Enums.DocStatusEnum.Printed);
+            if (!printedDocs.Any()) return;
+            MessageBox.Show($"Среднее время печати: {printedDocs.Average(s => s.PrintTime)} сек.");
+        }
+
+        private void BtnDocsInfo_Click(object sender, RoutedEventArgs e)
+        {
+            var printedDocs = PrintManagerModel.Docs.Where(s => s.Printed == Enums.DocStatusEnum.Printed);
+            if (!printedDocs.Any()) return;
+            MessageBox.Show($"Напечатанные документы:{Environment.NewLine}{string.Join(Environment.NewLine, printedDocs.Select(s => s.Name))}");
+
+        }
+    }
+}
